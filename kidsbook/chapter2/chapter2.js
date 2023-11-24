c3290133d23bd8b6bd4de30c2f2ecbc7a5366bf6
Desire Gwanzura <112183262+GuanKid@users.noreply.github.com>
2023-11-24 14:50:13 +0600

    Add files via upload

---
+++ kidsbook/chapter2/chapter2.js
@@ -0,0 +1,114 @@
+// console.log("This is a long string".slice(0, 4));
+// console.log(true && false);
+// console.log(12345 + 56789);
+// console.log(22 + 33 + 44);
+// console.log(1000 - 17);
+// console.log(123 * 456);
+// console.log(12345 / 250);
+// console.log(1234 + 57 * 3 - 31 / 4);
+// console.log(8 / 1 + 3);
+
+//Try out: page44
+
+// console.log((15+9)*2)
+
+
+
+//let age = 12;
+//let numberOfSiblings = 1 + 3;
+//let numberOfCandies = 8;
+//console.log(numberOfCandies / numberOfSiblings;)
+
+
+// let secondsInAMinute = 60;
+// let minutesInAnHour = 60;
+// let secondsInAnHour = secondsInAMinute * minutesInAnHour;
+// console.log(secondsInAnHour);
+
+// let hoursInADay = 24;
+// let secondsInADay = secondsInAnHour * hoursInADay;
+// console.log(secondsInADay);
+
+// let daysInAYear = 365;
+// let secondsInAYear = secondsInADay * daysInAYear;
+// console.log(secondsInAYear);
+
+// let age = 22;
+// console.log(age * secondsInAYear);
+
+
+
+// let highFives = 0;
+// console.log(++highFives);
+// console.log(++highFives);
+// console.log(--highFives);
+
+
+var x = 10;
+x = x + 5;
+console.log(x);
+
+let score = 10;
+score += 7;
+score -= 3;
+
+
+
+// //Try it Out: page 23
+// let balloons = 100;
+// baloons *= 2;
+// console.log(balloons)
+// balloons = 100
+// balloons /=4
+// console.log(balloons)
+
+
+
+let greeting = "Hello ";
+let myName = "Nick";
+console.log(greeting + myName);
+
+let java = "Java";
+console.log(java.length);
+
+let script = "Script";
+console.log(script.length);
+
+let javascript = java + script;
+console.log(javascript.length);
+
+myName = "Nick";
+console.log(myName[0]);
+console.log(myName[1]);
+console.log(myName[2]);
+
+let longString = "My long string is long";
+console.log(longString.slice(3));
+
+console.log("Hello there, how are you doing?".toUpperCase());
+console.log("hELlo THERE, hOW ARE yOu doINg?".toLowerCase());
+
+
+let sillyString = "hELlo THERE, hOW ARE yOu doINg?";
+let lowerString = sillyString.toLowerCase();
+let firstCharacter = lowerString[0];
+let firstCharacterUpper = firstCharacter.toUpperCase();
+let restOfString = lowerString.slice(1);
+console.log(firstCharacterUpper + restOfString);
+
+
+//Try out: page 37
+// let age = 12
+// let accompanied = true
+// let allowed = true
+// if (age <= 12) {
+//     if (accompanied === true) {
+//         allowed = true
+//     }
+//     else 
+//     {allowed = false}
+// }
+// else
+// {allowed = true}
+
+
