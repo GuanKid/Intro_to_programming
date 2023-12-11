6ec4631c8234a887ad15bd5335ed8a4a748bc751
Desire Gwanzura <112183262+GuanKid@users.noreply.github.com>
2023-12-11 22:34:58 +0600

    Add files via upload

---
+++ kidsbook/chapter2/chapter2.js
@@ -0,0 +1,85 @@
+// console.log("This is a long string".slice(0, 4));
+// console.log(true && false);
+// console.log(12345 + 56789);
+// console.log(22 + 33 + 44);
+// console.log(1000 - 17);
+// console.log(123 * 456);
+// console.log(12345 / 250);
+// console.log(1234 + 57 * 3 - 31 / 4);
+// console.log(8 / 1 + 3);
+
+//Try out: page44
+
+// console.log((15+9)*2)
+
+
+
+// let age = 12;
+// let numberOfSiblings = 1 + 3;
+// let numberOfCandies = 8;
+// console.log(numberOfCandies / numberOfSiblings)
+
+
+// let secondsInAMinute = 60;
+// let minutesInAnHour = 60;
+// let secondsInAnHour = secondsInAMinute * minutesInAnHour;
+// console.log(secondsInAnHour);
+
+// let hoursInADay = 24;
+// let secondsInADay = secondsInAnHour * hoursInADay;
+// console.log(secondsInADay);
+
+// let daysInAYear = 365;
+// let secondsInAYear = secondsInADay * daysInAYear;
+// console.log(secondsInAYear);
+
+// let age = 22;
+// console.log(age * secondsInAYear);
+
+
+
+// let highFives = 0;
+// console.log(++highFives);
+// console.log(--highFives);
+
+
+// var x = 10;
+// x = x + 5;
+// console.log(x);
+
+// let score = 10;
+// score += 7;
+// score -= 3;
+
+
+
+// //Try it Out: page 23
+// let balloons = 100;
+
+// balloons *=2;
+// console.log(balloons)
+
+// balloons = 100;
+// balloons /=4;
+// console.log(balloons)
+
+
+
+
+
+//Try out: page 37
+let age = 11
+let accompanied = true
+let allowed = true
+if (age < 12) {
+    if (accompanied === true) {
+        allowed = true
+    }
+    else 
+    {allowed = false}
+}
+else
+{allowed = true}
+
+
+console.log(allowed);
